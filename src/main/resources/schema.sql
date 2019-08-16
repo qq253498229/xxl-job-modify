00b6c559a8f82077a7f4d368e36c9946050719cc
王斌 <253498229@qq.com>
2019-08-16 21:31:41 +0800

    修改配置

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,4 +1,8 @@
-CREATE TABLE `xxl_job_info`
+CREATE database if NOT EXISTS `xxl_job` default character set utf8 collate utf8_general_ci;
+use `xxl_job`;
+
+
+CREATE TABLE if not exists `xxl_job_info`
 (
     `id`                        int(11)      NOT NULL AUTO_INCREMENT,
     `job_group`                 int(11)      NOT NULL COMMENT '执行器主键ID',
@@ -23,9 +27,10 @@ CREATE TABLE `xxl_job_info`
     `trigger_last_time`         bigint(13)   NOT NULL DEFAULT '0' COMMENT '上次调度时间',
     `trigger_next_time`         bigint(13)   NOT NULL DEFAULT '0' COMMENT '下次调度时间',
     PRIMARY KEY (`id`)
-);
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
 
-CREATE TABLE `xxl_job_log`
+CREATE TABLE if not exists `xxl_job_log`
 (
     `id`                        bigint(20) NOT NULL AUTO_INCREMENT,
     `job_group`                 int(11)    NOT NULL COMMENT '执行器主键ID',
@@ -43,11 +48,12 @@ CREATE TABLE `xxl_job_log`
     `handle_msg`                text COMMENT '执行-日志',
     `alarm_status`              tinyint(4) NOT NULL DEFAULT '0' COMMENT '告警状态：0-默认、1-无需告警、2-告警成功、3-告警失败',
     PRIMARY KEY (`id`),
-    KEY                         `I_trigger_time` (`trigger_time`),
-    KEY                         `I_handle_code` (`handle_code`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+    KEY `I_trigger_time` (`trigger_time`),
+    KEY `I_handle_code` (`handle_code`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
 
-CREATE TABLE `xxl_job_logglue`
+CREATE TABLE if not exists `xxl_job_logglue`
 (
     `id`          int(11)      NOT NULL AUTO_INCREMENT,
     `job_id`      int(11)      NOT NULL COMMENT '任务，主键ID',
@@ -57,9 +63,10 @@ CREATE TABLE `xxl_job_logglue`
     `add_time`    timestamp    NULL DEFAULT NULL,
     `update_time` timestamp    NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
 
-CREATE TABLE `xxl_job_registry`
+CREATE TABLE if not exists `xxl_job_registry`
 (
     `id`             int(11)      NOT NULL AUTO_INCREMENT,
     `registry_group` varchar(255) NOT NULL,
@@ -67,10 +74,11 @@ CREATE TABLE `xxl_job_registry`
     `registry_value` varchar(255) NOT NULL,
     `update_time`    timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`),
-    KEY              `i_g_k_v` (`registry_group`,`registry_key`,`registry_value`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+    KEY `i_g_k_v` (`registry_group`, `registry_key`, `registry_value`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
 
-CREATE TABLE `xxl_job_group`
+CREATE TABLE if not exists `xxl_job_group`
 (
     `id`           int(11)     NOT NULL AUTO_INCREMENT,
     `app_name`     varchar(64) NOT NULL COMMENT '执行器AppName',
@@ -79,9 +87,10 @@ CREATE TABLE `xxl_job_group`
     `address_type` tinyint(4)  NOT NULL DEFAULT '0' COMMENT '执行器地址类型：0=自动注册、1=手动录入',
     `address_list` varchar(512)         DEFAULT NULL COMMENT '执行器地址列表，多地址逗号分隔',
     PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
 
-CREATE TABLE `xxl_job_user`
+CREATE TABLE if not exists `xxl_job_user`
 (
     `id`         int(11)     NOT NULL AUTO_INCREMENT,
     `username`   varchar(50) NOT NULL COMMENT '账号',
@@ -90,26 +99,29 @@ CREATE TABLE `xxl_job_user`
     `permission` varchar(255) DEFAULT NULL COMMENT '权限：执行器ID列表，多个逗号分割',
     PRIMARY KEY (`id`),
     UNIQUE KEY `i_username` (`username`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
 
-CREATE TABLE `xxl_job_lock`
+CREATE TABLE if not exists `xxl_job_lock`
 (
     `lock_name` varchar(50) NOT NULL COMMENT '锁名称',
     PRIMARY KEY (`lock_name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
 
 
-INSERT INTO `xxl_job_group`(`id`, `app_name`, `title`, `order`, `address_type`, `address_list`)
+INSERT ignore INTO `xxl_job_group`(`id`, `app_name`, `title`, `order`, `address_type`, `address_list`)
 VALUES (1, 'xxl-job-executor-sample', '示例执行器', 1, 0, NULL);
-INSERT INTO `xxl_job_info`(`id`, `job_group`, `job_cron`, `job_desc`, `add_time`, `update_time`, `author`,
-                           `alarm_email`, `executor_route_strategy`, `executor_handler`, `executor_param`,
-                           `executor_block_strategy`, `executor_timeout`, `executor_fail_retry_count`, `glue_type`,
-                           `glue_source`, `glue_remark`, `glue_updatetime`, `child_jobid`)
+INSERT ignore INTO `xxl_job_info`(`id`, `job_group`, `job_cron`, `job_desc`, `add_time`, `update_time`, `author`,
+                                  `alarm_email`, `executor_route_strategy`, `executor_handler`, `executor_param`,
+                                  `executor_block_strategy`, `executor_timeout`, `executor_fail_retry_count`,
+                                  `glue_type`,
+                                  `glue_source`, `glue_remark`, `glue_updatetime`, `child_jobid`)
 VALUES (1, 1, '0 0 0 * * ? *', '测试任务1', '2018-11-03 22:21:31', '2018-11-03 22:21:31', 'XXL', '', 'FIRST',
         'demoJobHandler', '', 'SERIAL_EXECUTION', 0, 0, 'BEAN', '', 'GLUE代码初始化', '2018-11-03 22:21:31', '');
-INSERT INTO `xxl_job_user`(`id`, `username`, `password`, `role`, `permission`)
+INSERT ignore INTO `xxl_job_user`(`id`, `username`, `password`, `role`, `permission`)
 VALUES (1, 'admin', 'e10adc3949ba59abbe56e057f20f883e', 1, NULL);
-INSERT INTO `xxl_job_lock` (`lock_name`)
+INSERT ignore INTO `xxl_job_lock` (`lock_name`)
 VALUES ('schedule_lock');
 
 commit;
